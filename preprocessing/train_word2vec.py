4772df2c20bf696eea36ba8ceccf206dc75471c2
Bernhard Kratzwald <bernhard2202@gmail.com>
2016-06-29 16:48:52 +0200

    baseline computation

--- preprocessing/train_word2vec.py
+++ preprocessing/train_word2vec.py
@@ -4,9 +4,9 @@ from gensim.models.word2vec import Word2Vec
 DATA_PATH = '../data'
 TRAIN = os.path.join(DATA_PATH, 'train')
 TEST = os.path.join(DATA_PATH, 'test')
-POS_TWEET_FILE = os.path.join(TRAIN, 'train_pos_full.txt')
-NEG_TWEET_FILE = os.path.join(TRAIN, 'train_neg_full.txt')
-TEST_TWEET_FILE = os.path.join(TEST, 'test_data.txt')
+POS_TWEET_FILE = os.path.join(TRAIN, 'train_pos_full_orig.txt')
+NEG_TWEET_FILE = os.path.join(TRAIN, 'train_neg_full_orig.txt')
+TEST_TWEET_FILE = os.path.join(TEST, 'test_data_orig.txt')
 EMBEDDING_SIZE = 20
 
 def read_tweets(fname):
@@ -24,13 +24,11 @@ sentences = pos_tweets + neg_tweets + test_tweets
 print(len(sentences))
 
 tokens = [item.strip() for sentence in sentences for item in sentence]
-# Check for Nikos's 1st stage substitutions.
-assert '<num>' in tokens
 
 WORKERS = 8
 model = Word2Vec(sentences, size=EMBEDDING_SIZE, window=10, min_count=5, workers=WORKERS)
 
-fname = "{0}/word2vec/word2vec-local-gensim-{1}.bin".format(DATA_PATH, EMBEDDING_SIZE)
+fname = "{0}/word2vec/word2vec-local-gensim-orig-{1}.bin".format(DATA_PATH, EMBEDDING_SIZE)
 print("Writing embeddings to file {0}.".format(fname))
 model.save(fname)
 print("Done! Happy neural networking!")
